de3320c3ec0d05dc56399c7cfccf46a67a0c1745
chukky65 <ogachuchu65@gmail.com>
2021-06-06 20:42:10 +0100

    Adjust some Data types

---
+++ main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+   var Dot: Float=45.34
+    var firstnum: Int=4
+    var surname: String="chuks okafor"
+    var single:Char='d'
+    var logic:Boolean= true
+    var decimal: Double=47663.38774
+    var small:Short=365516
+    var large:Long=837362677272667428173
+    var list:Array=("mobile dev", "ui/ux", "backend/frontend")
+    var ran:Byte=112
+    println(Dot)
+    println(firstnum)
+    println(surname)
+    println(single)
+    println(logic)
+    println(decimal)
+    println(small)
+    println(large)
+    println(list)
+    println(ran)
+
+
+}
